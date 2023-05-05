dbebb5610fb7944fdeb72c685e33345fa9d32b7b
王浩 <237466265@qq.com>
2023-05-05 09:35:44 +0800

    lab6 finished

---
+++ lab/lab06/lab06.py
@@ -0,0 +1,89 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    a -= 1
+    def f(x):
+        nonlocal a
+        a = a + 1
+        return a + x
+    return f
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    time = 0
+    def fib():
+        nonlocal time
+        time += 1
+        def helper(i):
+            if i <= 1:
+                return 0
+            elif i == 2:
+                return 1
+            return helper(i - 1) + helper(i - 2)
+        return helper(time)
+    return fib
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    idx_list = []
+    for i in range(0, len(lst)):
+        if lst[i] == entry:
+            idx_list.append(i)
+    i = 0
+    for x in idx_list:
+        lst.insert(x + 1 + i, elem)
+        i += 1
+    return lst
